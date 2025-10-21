0532482d30c87b74f63301313fd700a6c9b7fe4d
Namig <namig.pashayev1@gmail.com>
2025-10-21 16:04:27 +0400

    Update Home page and create SolarSystem component

---
+++ src/app/components/SolarSystem.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import styles from './solar-system.module.scss';
+
+const SolarSystem = () => {
+    return (
+        <div className={styles.main}>
+            <div className={styles.rocketOrbit}>
+                <Image
+                    className={styles.rocket}
+                    src="/images/rocket.png"
+                    alt='rocket' width={1024}
+                    height={1024}
+                />
+            </div>
+
+            <div className={styles.solarSystem}>
+                <div className={`${styles.sun} ${styles.circle}`}></div>
+                <div className={`${styles.planet1} ${styles.circle}`}>
+                    <div className={`${styles.orbit1} ${styles.orbit} ${styles.circle}`}>
+                        <div className={`${styles.satellite1} ${styles.circle}`}></div>
+                    </div>
+                </div>
+                <div className={`${styles.planet2} ${styles.circle}`}>
+                    <div className={`${styles.orbit2} ${styles.orbit} ${styles.circle}`}>
+                        <div className={`${styles.satellite2} ${styles.circle}`}></div>
+                    </div>
+                    <div className={`${styles.orbit3} ${styles.orbit} ${styles.circle}`}>
+                        <div className={`${styles.satellite3} ${styles.circle}`}></div>
+                    </div>
+                    <div className={`${styles.orbit4} ${styles.orbit} ${styles.circle}`}>
+                        <div className={`${styles.satellite4} ${styles.circle}`}></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SolarSystem;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
+import SolarSystem from "./components/SolarSystem";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-
-    </div>
+    <main className={styles.page}>
+      <SolarSystem />
+      <div className={styles.txt}>
+        <div className={styles.headingTxt}>
+          <Image src="/images/galaxy.png" alt="galaxy icon" width={512} height={512} className={styles.galaxyIcn} />
+          <h1>
+            Explore the Universe of Web Development
+          </h1>
+        </div>
+        <p>
+          Start your journey through the cosmos of code — one planet at a time.
+        </p>
+        <p>
+          Learn the building blocks of the web — HTML, CSS, and JavaScript — through interactive planets, visual guides, and hands-on examples made for absolute beginners.
+        </p>
+      </div>
+    </main>
   );
 }
